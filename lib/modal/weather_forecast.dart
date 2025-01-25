0e202e116ff23e0f10f384d921013641b85b4990
Esther Jenslin <esther@katenterprise.com>
2025-01-26 01:44:49 +0530

    task completed

--- lib/modal/weather_forecast.dart
+++ lib/modal/weather_forecast.dart
@@ -1,19 +1,4 @@
-/*class WeatherForecast {
-  final DateTime dateTime;
-  final double temp;
 
-  WeatherForecast({
-    required this.dateTime,
-    required this.temp,
-  });
-
-  factory WeatherForecast.fromJson(Map<String, dynamic> json) {
-    return WeatherForecast(
-      dateTime: DateTime.parse(json['dt_txt']),
-      temp: json['main']['temp'].toDouble(),
-    );
-  }
-}*/
 
 class WeatherForecast {
   final double temperature;
--- lib/modal/weather_response.dart
+++ lib/modal/weather_response.dart
@@ -1,6 +1,5 @@
 import 'package:weather_task/modal/weather_forecast.dart';
 
-
 class WeatherResponse {
   final String cityName;
   final String country;
@@ -22,29 +21,3 @@ class WeatherResponse {
     );
   }
 }
-/*
-class WeatherResponsee {
-  final String cityName;
-  final double temperature;
-  final String weatherDescription;
-  final List<WeatherForecast> forecasts;
-
-  WeatherResponse({
-    required this.cityName,
-    required this.temperature,
-    required this.weatherDescription,
-    required this.forecasts,
-  });
-
-  factory WeatherResponse.fromJson(Map<String, dynamic> json) {
-    return WeatherResponse(
-      cityName: json['name'],
-      temperature: json['main']['temp'].toDouble(),
-      weatherDescription: json['weather'][0]['description'],
-      forecasts: (json['list'] as List)
-          .map((forecast) => WeatherForecast.fromJson(forecast))
-          .toList(),
-    );
-  }
-}
-*/
